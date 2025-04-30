533126d89a42e7cfaa7a0c7d92c3dccba4bc82e6
fatmahussein <fartunhussein346@gmail.com>
2025-04-30 20:18:57 +0300

    login page succcessfully done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      colors: {
+        primary: '#5f6368', 
+        secondary: '#ff5722', 
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        syne: ['Syne', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
